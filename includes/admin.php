3d2dc914d085129399a308a76178d50627defd5e
useStrict <useStrict@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-09 12:19:59 +0000

    partial save
    git-svn-id: https://plugins.svn.wordpress.org/eshop-shipping-extension/branches/1.4@583502 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin.php
+++ includes/admin.php
@@ -47,7 +47,6 @@ class USC_eShop_Shipping_Extension_Admin extends USC_eShop_Shipping_Extension
 		if ( $hook == 'post-new.php' || $hook == 'post.php' ) 
 		{
 			$eshop_post_types = apply_filters('eshop_post_types',array('post','page'));
-			error_log(__LINE__ . ' ' . print_r($eshop_post_types,1));
 			
 			if (in_array($post->post_type, $eshop_post_types))
 			{
